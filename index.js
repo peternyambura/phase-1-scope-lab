ddb09428b5a5de3bb4fcc6a9d4e1f485be97bb23
= <peterndirangu0005@gmail.com>
2023-07-17 05:56:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'someone';
+  leastFavoriteCustomer = 'new name'; 
+}
+
+module.exports = {
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer,
+};
